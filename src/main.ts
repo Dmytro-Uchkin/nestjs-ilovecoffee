085d6442b190b919304843846aa7ca1c4bfda182
Dmytro Uchkin <dmytro.uchkin@gmail.com>
2024-05-20 19:10:35 +0100

    Generating OpenAPI Specification - Introducing the Swagger Module - Enabling CLI Plugin - Decorating Model Properties

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
@@ -20,6 +21,15 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('nestjs-ilovecoffee')
+    .setDescription('Coffee Application')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
